819acdf8d0082f5bfac71f923fbfa7e8c8da40bd
Anurag Basuri <anuragbasuri2502@gmail.com>
2025-07-23 20:23:06 +0530

    feat: Refactor auth page code and update routing to include auth component

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -6,10 +6,13 @@ import Team from '../pages/team.jsx';
 import Social from '../pages/socials.jsx';
 import Contact from '../pages/contact.jsx';
 import MemberProfile from '../pages/member.jsx';
+import Auth from '../pages/auth.jsx';
 
 const AppRoutes = () => {
     return (
         <Routes>
+            <Route path="/auth/*" element={<Auth />} />
+
             <Route path="/" element={<Home />} />
             <Route path="/event" element={<Event />} />
             <Route path="/team" element={<Team />} />
